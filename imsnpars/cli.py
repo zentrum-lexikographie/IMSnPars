5b085383860bd0c0f8e164947ef5872c348b20d8
rknaebel <rknaebel@uni-potsdam.de>
2020-08-14 17:01:08 +0200

    fix parser call in click application

--- imsnpars/cli.py
+++ imsnpars/cli.py
@@ -1,8 +1,13 @@
-import click
-import conllu
+import argparse
 import itertools
 import multiprocessing
+import sys
 
+import click
+import conllu
+
+import imsnpars.nparser.options as parser_options
+import imsnpars.tools.utils as parser_utils
 from imsnpars.nparser import builder as parser_builder
 from imsnpars.tools.utils import ConLLToken as IMSConllToken
 
@@ -25,12 +30,13 @@ class Parser(object):
                     tokId=(i + 1),
                     orth=token['form'],
                     lemma=token['lemma'],
-                    pos=token['xpos'], langPos=token['xpos'],
+                    pos=token['xpostag'],
+                    langPos=token['xpostag'],
                     morph='',
                     headId=None,
                     dep=None,
-                    norm=None  # FIXME: normalize tokens?
-                ) for i, token in sentence]
+                    norm=parser_builder.buildNormalizer(self.options.normalize)
+                ) for i, token in enumerate(sentence)]
             )
         )
         for pos, token in enumerate(sentence):
@@ -41,24 +47,40 @@ class Parser(object):
         return sentence
 
 
-def parser_stub(sentence):
-    return sentence
+def build_parser_from_args(cmd_args, parser, model):
+    argParser = argparse.ArgumentParser(description="""IMS BiLSTM Parser""", add_help=False)
+    argParser.add_argument("--parser", help="which parser to use", choices=["GRAPH", "TRANS"], required=True)
+    argParser.add_argument("--normalize", help="normalize the words", type=bool, default=True)
+    argParser.add_argument("--model", help="load model from the file", type=str, required=True)
+    # parse the second time to get all the args
+    parser_options.addParserCmdArguments(parser, argParser)
+    args, _ = argParser.parse_known_args(cmd_args)
+
+    opts = parser_utils.NParserOptions()
+    parser_options.fillParserOptions(args, opts)
+    opts.load(model + ".args")
+    opts.logOptions()
+
+    return args, opts
 
 
 @click.command()
 @click.option('-b', '--batch', default=1000, type=int)
 @click.option('-i', '--input', default='-', type=click.File('r'))
 @click.option('-o', '--output', default='-', type=click.File('w'))
-def main(input, output, batch):
-    # FIXME: translate CLI args, options into parser parameters
-    # parser = Parser(None, None)
-    parser = parser_stub
+@click.option('-m', '--model', type=str)
+@click.option('-p', '--parser', default="TRANS", type=str)
+@click.option('-j', '--jobs', default=1, type=int)
+def main(input, output, batch, model, parser, jobs):
+    args = sys.argv[1:] + ["--normalize", "True", "--parser", parser, "--model", model]
+    args, options = build_parser_from_args(cmd_args=args, parser=parser, model=model)
+    parser = Parser(args, options)
 
     # We provide the field definition explicitly, so the parser does not seek
     # through the input stream, looking for a field declaration. The latter
     # will fail on piped stdin.
     sentences = conllu.parse_incr(input, fields=conllu.parser.DEFAULT_FIELDS)
-    with multiprocessing.Pool() as p:
+    with multiprocessing.Pool(jobs) as p:
         while True:
             chunk = list(itertools.islice(sentences, batch))
             if len(chunk) == 0:
