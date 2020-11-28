9ca61d80bab99f1ff96d3ae97ca8aa3e8f36594c
UH <554c46@gmail.com>
2020-11-28 14:03:56 +0100

    path to package corrected

--- imsnpars/main.py
+++ imsnpars/main.py
@@ -10,9 +10,9 @@ import sys
 import random
 import argparse
 
-from .tools import utils, evaluator, training
-from .nparser import builder
-from .nparser import options
+from imsnpars.tools import utils, evaluator, training
+from imsnpars.nparser import builder
+from imsnpars.nparser import options
 
 def buildParserFromArgs():
     argParser = argparse.ArgumentParser(description="""IMS Neural Parser""", add_help=False)
